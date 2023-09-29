0c73e3760a791effe04d249221b0dea353c05ff9
Gresa Smolica <gresasmolica@gmail.com>
2023-09-29 10:18:41 +0200

    edits in tensorflow

--- network_tensorflow.py
+++ network_tensorflow.py
@@ -11,28 +11,21 @@ class NeuralNetworkTf(tf.keras.Sequential):
     tf.random.set_seed(random_state)
     
     for i in range(0, len(sizes)):
-
       if i == len(sizes) - 1:
         self.add(tf.keras.layers.Dense(sizes[i], activation='softmax'))
       else:
         self.add(tf.keras.layers.Dense(sizes[i], activation='sigmoid'))
-
-  def compile_and_fit(self, x_train, y_train, epochs=50, learning_rate=0.01, batch_size = 1, validation_data=None):
-    
-    optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
-    loss_function = tf.keras.losses.CategoricalCrossentropy()
-    eval_metrics = ['accuracy']
-
-    self.compile(optimizer=optimizer, loss=loss_function, 
-                 metrics=eval_metrics)
-    
-    if validation_data:
-      return self.fit(x_train, y_train, epochs=epochs, 
-                      batch_size=batch_size, 
-                      validation_data=validation_data)
-    else:
-      return self.fit(x_train, y_train, epochs=epochs, 
-                      batch_size=batch_size)
+        
+  def predict(self, x, batch_size=None, verbose="auto", steps=None, callbacks=None, max_queue_size=10, workers=1, use_multiprocessing=False):
+    return super().predict(x, batch_size, verbose, steps, callbacks, max_queue_size, workers, use_multiprocessing)
+       
+  def compute_accuracy(self, x, y):
+    correct_predictions = 0
+    for i in range(len(x)):
+      output = self(x[i])
+      if tf.argmax(output) == tf.argmax(y[i]):
+        correct_predictions += 1
+    return correct_predictions / len(x) 
 
 class TimeBasedLearningRate(tf.keras.optimizers.schedules.LearningRateSchedule):
   '''TODO: Implement a time-based learning rate that takes as input a 
@@ -40,21 +33,47 @@ class TimeBasedLearningRate(tf.keras.optimizers.schedules.LearningRateSchedule):
   learning rate by 1 until minimal learning rate of 1 is reached.
     '''
 
-  def __init__(self, initial_learning_rate):
+  def __init__(self, initial_learning_rate, decay_factor=0.9):
     super().__init__()
     self.initial_learning_rate = initial_learning_rate
     self.min_learning_rate = 1
+    self.decay_factor = decay_factor
 
   def __call__(self, step):
-    return tf.maximum(self.initial_learning_rate - step, self.min_learning_rate)
+    learning_rate = self.initial_learning_rate * (self.decay_factor ** step)
+    if learning_rate < self.min_learning_rate:
+      learning_rate = self.min_learning_rate
+    return learning_rate
   
   def get_config(self):
     return {'initial_learning_rate': self.initial_learning_rate}
+  
+  def compile_and_fit(self, x_train, y_train,
+                      validation_data=None, epochs=50, learning_rate=0.01,
+                      batch_size=1):
+     optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
+     loss_function = tf.keras.losses.CategoricalCrossentropy()
+     eval_metrics = ['accuracy']
+     
+     history = {'loss': [], 'accuracy': [], 'val_loss': [], 'val_accuracy': []}
+     
+     for iteration in range(epochs):
+      train_accuracy = self.compute_accuracy(x_train, y_train)
+      val_accuracy = self.compute_accuracy(validation_data[0], validation_data[1])
+      history['accuracy'].append(train_accuracy)
+      history['val_accuracy'].append(val_accuracy)
+      
+      for x, y in zip(x_train, y_train):
+        with tf.GradientTape() as tape:
+          output = self(x)
+          loss = loss_function(y, output)
+          gradients = tape.gradient(loss, self.trainable_variables)
+          optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+     return history
   
   '''Commenting on the mistakes in the Network class:
   1. The activation function in the Network class: Considering that we are dealing with a multi-class classification problem, the activation function of the output layer should be softmax, whearas the activation function of the hidden layers can be either sigmoid or relu. The softmax activation function is usually implemented in the output layers as its result can be interpreted as a probability distribution.
   2. The loss function in the Network class: As we are dealing with a multi-class classification problem, the most appropriate loss function would be categorical crossentropy. The current loss function - binary crossentropy is used for binary classification problems and if implemented in a multi-class classification problem, it would result in a very high loss value and would not allow the model to learn or perform well.
   3. Validation data: Initially, the validation data argument was not passed into the fit method. This would result in the model not being validated and consecutively not allowing the model to learn well.'''
-
 
     
